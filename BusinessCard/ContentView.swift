6559512cd3c3ef5d83954be31b8c8d99a9963ccd
thisisstreamliner <thisisstreamliner@gmail.com>
2020-10-29 11:33:33 +0200

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Tiger Mei on 29.10.2020.
+//  Copyright © 2020 Tiger Mei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
